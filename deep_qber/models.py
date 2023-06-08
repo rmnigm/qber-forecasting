e5057156f8c962d4ceb9fd7617b972030d3c20a5
Roma Nigmatullin <70426991+rmnigm@users.noreply.github.com>
2023-06-08 03:06:21 +0300

    added base autoregressive models
    added base autoregressive models:
    * autoregressive dense net with exod data from other ts
    * pure autoregressive dense net
    * LSTM-based model for multivariate ts

--- deep_qber/models.py
+++ deep_qber/models.py
@@ -15,3 +15,85 @@ class ExpSmoothing:
 
     def get(self):
         return self.val
+
+
+class Extractor(nn.Module):
+    def __init__(self, look_back, output_size, hidden_size):
+        super().__init__() 
+        self.input_size = look_back
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.autoregressive_dense = nn.Sequential(
+            nn.Linear(self.input_size, self.hidden_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.hidden_size, self.hidden_size),
+        )
+        self.classifier = nn.Sequential(
+            nn.LeakyReLU(),
+            nn.Linear(self.hidden_size, self.output_size)
+        )
+        
+    def forward(self, data):
+        x, x_current = data
+        autoregressive_features = self.autoregressive_dense(x[:, :, 0])
+        return self.classifier(autoregressive_features)
+
+
+class ExtractorExod(nn.Module):
+    def __init__(self, look_back, output_size, hidden_size):
+        super().__init__() 
+        self.input_size = look_back
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.autoregressive_dense = nn.Sequential(
+            nn.Linear(self.input_size, self.hidden_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.hidden_size, self.hidden_size),
+        )
+        self.dense_exod = nn.Sequential(
+            nn.Linear(6, self.hidden_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.hidden_size, self.hidden_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.hidden_size, 64),
+            )
+        self.classifier = nn.Sequential(
+            nn.LeakyReLU(),
+            nn.Linear(self.hidden_size + 64, self.output_size)
+        )
+        
+    def forward(self, data):
+        x, x_current = data
+        autoregressive_features = self.autoregressive_dense(x[:, :, 0])
+        exod_features = self.dense_exod(x_current)[:, -1, :]
+        return self.classifier(torch.cat((autoregressive_features, exod_features), 1))
+    
+    
+class ExtractorLSTM(nn.Module):
+    def __init__(self, input_size, output_size, hid_size=128):
+        super().__init__() 
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hid_size = hid_size
+        self.lstm = nn.LSTM(input_size,
+                            hid_size,
+                            batch_first=True
+                            )
+        self.dense = nn.Sequential(
+            nn.Linear(input_size - 1, hid_size),
+            nn.LeakyReLU(),
+            nn.Linear(hid_size, hid_size),
+        )
+        self.regressor = nn.Linear(2 * hid_size, output_size)
+        
+    def forward(self, data):
+        x, x_current = data
+        x, _ = self.lstm(x)
+        past_features = x[:, -1, :]
+        current_features = self.dense(x_current)[:, -1, :]
+        features = torch.cat((past_features, current_features), 1)
+        return self.regressor(features)
